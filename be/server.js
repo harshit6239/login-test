b9fc9431bf6ccc96d319199b5ff08fbe3b7abbd9
Harshit Manchanda <128061019+harshit6239@users.noreply.github.com>
2025-05-18 13:44:13 +0530

    allowed origins

--- be/server.js
+++ be/server.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const corsOptions = {
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.ALLOWED_ORIGINS,
     credentials: true,
     methods: ["GET", "POST"],
 };
